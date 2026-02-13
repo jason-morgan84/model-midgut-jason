dd1b68b87ac24cc2a263f7c7c97bdbae447af94a
Jason <jason_morgan84@yahoo.co.uk>
2026-02-13 14:58:04 +0000

    Deleted old testing files

--- Moving test.py
+++
@@ -1,8 +0,0 @@
-import math
-
-print(math.atan2(1,1)*180/math.pi)
-print(math.atan2(-1,1)*180/math.pi)
-print(math.atan2(1,-1)*180/math.pi)
-print(math.atan2(0,-1)*180/math.pi)
-print(math.atan2(-.01,-1)*180/math.pi)
-print(math.atan2(-1,-1)*180/math.pi)
